eb559c7b5c1802f70745fb7385939f67d25a6924
daniel <danielzhang>
2019-02-21 07:12:54 +0800

    Add desc for no.387

--- no387/FirstUniqueCharacterInAString.java
+++ no387/FirstUniqueCharacterInAString.java
@@ -1,13 +1,28 @@
 package com.daniel.leetcode.solutions.no387;
 
+/*
+    Given a string, find the first non-repeating character in it and return it's index. If it doesn't exist, return -1.
+
+    Examples:
+
+    s = "leetcode"
+    return 0.
+
+    s = "loveleetcode",
+    return 2.
+ */
+
 public class FirstUniqueCharacterInAString {
 
-    public int firstUniqChar(String s) {
+    public static int firstUniqChar(String s) {
 
         int[] freq = new int[26];
 
-        for (int i = 0; i < s.length(); i ++)
+        for (int i = 0; i < s.length(); i ++) {
+            System.out.println(s.charAt(i));
+            System.out.println(s.charAt(i) - 'a');
             freq[s.charAt(i) - 'a'] ++;
+        }
         for (int i = 0; i < s.length(); i ++) {
             if (freq[s.charAt(i) - 'a'] == 1)
                 return i;
@@ -15,5 +30,9 @@ public class FirstUniqueCharacterInAString {
 
         return -1;
 
+    }
+
+    public static void main(String[] args) {
+        firstUniqChar("danielzhangdanie");
     }
 }
